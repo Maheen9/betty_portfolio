73cf1aea537ebf32d5a688898bb1e10d8384c7fb
maheen9 <maheen97.fatima@gmail.com>
2022-05-01 11:22:36 +0500

    commit

--- portfolio/mail.php
+++ portfolio/mail.php
@@ -1,4 +1,6 @@
 <?php
+
+
     $fname= $_POST['first_name'];
     $lname= $_POST['last_name'];
     $email= $_POST['email'];
@@ -11,6 +13,6 @@
             "Email: $email.\n".
             "Message: $msg.\n";
     $to="trialaccn911@gmail.com";
-    $mail($to,$subject,$body);
+    $mail($to, $subject, $body);
     header("Location: index.html");
 ?>
